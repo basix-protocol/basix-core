7d197d39cb24f4b06e70b016324e3fbed23b668a
SkullElephant <skullelephant@protonmail.com>
2021-02-12 11:22:05 +0100

    feat: 🎸 Add team lock and remove sale

---
+++ contracts/BasixTeamLock.sol
@@ -0,0 +1,54 @@
+/*
+     ___    _____  ___    _  _    _ 
+    (  _`\ (  _  )(  _`\ (_)( )  ( )
+    | (_) )| (_) || (_(_)| |`\`\/'/'
+    |  _ <'|  _  |`\__ \ | |  >  <  
+    | (_) )| | | |( )_) || | /'/\`\ 
+    (____/'(_) (_)`\____)(_)(_)  (_)
+*/
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract BasixTeamLock {
+
+    IERC20 private _basix;
+    address private _beneficiary;
+    uint256 private _releaseTime;
+
+    constructor (IERC20 basix_, uint256 releaseTime_) {
+        _basix = basix_;
+        _beneficiary = msg.sender;
+        _releaseTime = releaseTime_;
+    }
+
+    /**
+     * @return the basix address.
+     */
+    function basix() public view returns (IERC20) {
+        return _basix;
+    }
+
+    /**
+     * @return the beneficiary of the tokens.
+     */
+    function beneficiary() public view returns (address) {
+        return _beneficiary;
+    }
+
+    /**
+     * @return the time when the Basix tokens are released.
+     */
+    function releaseTime() public view returns (uint256) {
+        return _releaseTime;
+    }
+
+    /**
+     * Withdraw the team tokens when ready.
+     */
+    function withdraw() external {
+        require(block.timestamp == releaseTime(), "BasixTeamLock: tokens are still locked");
+        uint256 amount = basix().balanceOf(address(this));
+        require(amount > 0, "BasixTeamLock: tokens already released");
+        basix().transfer(beneficiary(), amount);
+    }
+}
